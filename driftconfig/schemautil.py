a58f9b8ad928020e20f1d8e20964971cc4acb1eb
Matthias Gudmundsson <mattigumm@gmail.com>
2017-01-10 13:01:53 +0000

    Disabling format check in json schema temporarily.

--- driftconfig/schemautil.py
+++ driftconfig/schemautil.py
@@ -16,7 +16,8 @@ log = logging.getLogger(__name__)
 def check_schema(json_object, schema, title=None):
     """Do json schema check on object and abort with 400 error if it fails."""
     try:
-        jsonschema.validate(json_object, schema, format_checker=jsonschema.FormatChecker())
+        ###jsonschema.validate(json_object, schema, format_checker=jsonschema.FormatChecker())
+        jsonschema.validate(json_object, schema)
     except jsonschema.ValidationError as e:
         report = _generate_validation_error_report(e, json_object)
         if title:
