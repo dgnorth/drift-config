9571359c265581d6857a00ffba8d524c625de7b9
Matthias Gudmundsson <mattigumm@gmail.com>
2017-03-27 18:18:48 +0000

    Fixing up primary keys in tenants tables. Renaming 'version' to 'release' in 'deployables' table.

--- driftconfig/config.py
+++ driftconfig/config.py
@@ -450,7 +450,7 @@ def get_drift_table_store():
     deployables.add_schema({
         'type': 'object',
         'properties': {
-            'version': {'type': 'string'},
+            'release': {'type': 'string'},
             'is_active': {'type': 'boolean'},
         },
         'required': ['is_active'],
@@ -472,9 +472,12 @@ def get_drift_table_store():
     products.add_default_values({'state': 'active', 'deployables': []})
 
     tenant_names = ts.add_table('tenant-names')
-    tenant_names.add_primary_key('tenant_name')
+    tenant_names.add_primary_key('tenant_name,product_name')
+    tenant_names.set_row_as_file(subfolder_name='tenants', group_by='product_name')
+    tenant_names.add_unique_constraint('tenant_name')
     tenant_names.add_foreign_key('product_name', 'products')
     tenant_names.add_foreign_key('organization_name', 'organizations')
+    tenant_names.add_foreign_key('tier_name', 'tiers')
     tenant_names.add_schema({
         'type': 'object',
         'properties': {
@@ -482,14 +485,13 @@ def get_drift_table_store():
             'reserved_at': {'format': 'date-time'},
             'reserved_by': {'type': 'string'},
         },
-        'required': ['product_name', 'organization_name'],
+        'required': ['product_name', 'organization_name', 'tier_name'],
     })
     tenant_names.add_default_values({'reserved_at': '@@utcnow'})
 
     tenants = ts.add_table('tenants')
-    tenants.add_primary_key('tier_name,deployable_name,tenant_name')
-    tenants.set_row_as_file(subfolder_name=tenants.name, group_by='tier_name,tenant_name')
-    tenants.add_foreign_key('tier_name', 'tiers')
+    tenants.add_primary_key('deployable_name,tenant_name')
+    tenants.set_row_as_file(subfolder_name='tenants', group_by='tenant_name')
     tenants.add_foreign_key('deployable_name', 'deployable-names')
     tenants.add_foreign_key('tenant_name', 'tenant-names')
     tenants.add_schema({
