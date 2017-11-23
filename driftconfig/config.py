5e9e17c13736bf835ef90cee2d8138b254989e80
Matthias Gudmundsson <matti@directivegames.com>
2017-11-23 10:20:26 +0000

    Removing unique constraint on 'alias' field in tenant names table as the constraints doesn't support null values.

--- driftconfig/config.py
+++ driftconfig/config.py
@@ -479,7 +479,7 @@ def get_drift_table_store():
         tenant_names.add_primary_key('tenant_name')
         tenant_names.add_foreign_key('product_name', 'products')
         tenant_names.add_foreign_key('organization_name', 'organizations')
-        tenant_names.add_unique_constraint('alias')
+        # this constraint doesn't support null values: tenant_names.add_unique_constraint('alias')
         tenant_names.add_schema({
             'type': 'object',
             'properties': {
