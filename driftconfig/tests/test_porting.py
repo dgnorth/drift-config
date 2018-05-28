8d9c73464d8e3a73736873cf596eae22b99d1010
Kristján Valur Jónsson <sweskman@gmail.com>
2018-05-28 19:56:36 +0000

    Add import tests for scripts to catch py3k syntax errors

---
+++ driftconfig/tests/test_porting.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import sys
+import unittest
+import os.path
+
+
+"""
+Test the import of various top level modules to make sure they compile
+"""
+class ImportTestCase(unittest.TestCase):
+    def test_driftconfig(self):
+        import driftconfig
+
+
+class ScriptImportTestCase(unittest.TestCase):
+    script_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "scripts"))
+
+    def setUp(self):
+        sys.path.append(self.script_dir)
+    def tearDown(self):
+        sys.path.pop()
+
+    def test_testconfig(self):
+        import testconfig
+
+    def test_update_trigger(self):
+        # must use this because the module contains a dash
+        __import__('update-trigger', globals(), locals(), [], 0)
