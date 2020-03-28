3da360670e063f5886d64e7c45ac1f0a49632284
Jon Bjarnason <jonb@wuxinextcode.com>
2020-03-28 16:15:11 +0000

    travis

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-import re
-import ast
+import os
 from setuptools import setup
 
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
+version_file = os.path.abspath(os.path.join("driftconfig", "VERSION"))
 
-
-with open('driftconfig/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+with open(version_file) as f:
+    version = f.readlines()[0].strip()
 
 
 setup(
